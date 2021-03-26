8b09e8245df17ed67a43093d66ac53e3be22908e
Puneet Daga <pdaga2@gmail.com>
2021-03-26 18:39:52 +0530

    Update Hack2021Dao.java

--- hack2021Demo/src/main/java/com/amazon/hack2021Demo/dao/Hack2021Dao.java
+++ hack2021Demo/src/main/java/com/amazon/hack2021Demo/dao/Hack2021Dao.java
@@ -14,7 +14,7 @@ public class Hack2021Dao {
 
     private static final String FILE_PATH_1 = "/Users/dagapune/Downloads/hack2021Demo/src/main/resources/static/test.csv";
     
-    public List<Stream> getStreamDataByVcid(String vcid) throws Exception {
+    public List<Stream> getStreamDataByAsin(String asin) throws Exception {
         //TODO write logic to map csv data with vcid id
         return new CsvToBeanBuilder(new FileReader(FILE_PATH_1))
         .withType(Stream.class).build().parse(); 
