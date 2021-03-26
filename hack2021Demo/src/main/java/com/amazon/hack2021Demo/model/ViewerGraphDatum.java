0af10ea3879ae7d96b5b18046395d3de02bdd202
Puneet Daga <pdaga2@gmail.com>
2021-03-26 19:08:32 +0530

    Update ViewerGraphDatum.java

--- hack2021Demo/src/main/java/com/amazon/hack2021Demo/model/ViewerGraphDatum.java
+++ hack2021Demo/src/main/java/com/amazon/hack2021Demo/model/ViewerGraphDatum.java
@@ -2,11 +2,13 @@ package com.amazon.hack2021Demo.model;
 
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class ViewerGraphDatum {
-    private String vcid;
+    private String asin;
     private int maxScore;
     private List<Integer> strengthPoints;
 }
