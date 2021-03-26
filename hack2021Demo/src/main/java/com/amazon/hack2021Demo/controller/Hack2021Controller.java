edcdbb1f55f2d11ac46361590ade1f097e9b60a2
Puneet Daga <pdaga2@gmail.com>
2021-03-26 19:09:34 +0530

    Update Hack2021Controller.java

--- hack2021Demo/src/main/java/com/amazon/hack2021Demo/controller/Hack2021Controller.java
+++ hack2021Demo/src/main/java/com/amazon/hack2021Demo/controller/Hack2021Controller.java
@@ -4,17 +4,20 @@ import com.amazon.hack2021Demo.model.ViewerGraphDatum;
 import com.amazon.hack2021Demo.service.Hack2021Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/demo")
 public class Hack2021Controller {
 
     @Autowired
     private Hack2021Service hack2021Service;
 
-    @GetMapping(value = "/asin/{asin}")
+    @GetMapping(value = "/asin/{asin}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ViewerGraphDatum createGraphByAsin(@PathVariable("asin") String asin) {
         return hack2021Service.createGraphByAsin(asin);
     }
